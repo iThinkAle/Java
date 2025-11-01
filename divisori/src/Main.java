dad4a6a2f21bfa8cb3935a8641c591c6cdd4796d
iThinkAle <alessandrorico@outlook.it>
2025-11-02 00:54:49 +0100

    Commit

--- divisori/src/Main.java
+++ divisori/src/Main.java
@@ -11,6 +11,7 @@ class Main {
 
         long n = 0L;
 
+        //reads input from user
         System.out.println("Inserisci un numero");
         try{
             n = Long.parseLong(tastiera.readLine());
@@ -19,8 +20,10 @@ class Main {
             return;
         }
 
+        //declares dynamic array "divs"
         List<Long> divs = new ArrayList<>();
 
+        //calculates square root of n
         long rad = (long) Math.sqrt(n);
 
         /*
@@ -28,25 +31,31 @@ class Main {
           instead of doing one cycle to compute divisors from 1 to n (it would be much slower for bigger numbers), we divide the
           computation into two cycles.
           one cycle goes from 1 to sqrt(n). this works because every i divisor of n has a complementary divisor n/i, so to compute every divisor
-          we can go from 1 to only sqrt(n), because using that we can already get every divisor.
+          we can go from 1 to only sqrt(n), since using that we can already get every divisor.
           second cycle is optional. it goes from rad to n to compute divisors bigger than sqrt(n) and
           backwards to add them to the dynamic array in crescent order.
          */
 
-        //cycle for numbers from 1 to rad
+        //measures elapsed time
+        long start = System.nanoTime();
+
+        //computes divisors of n from 1 to rad
         for(long i = 1; i <= rad; i++){
             if(n % i == 0){
                 divs.add(i);
             }
         }
 
-        //cycle for numbers from rad to n. reversed to add them to divs in crescent order
+        //computes divisors for n from rad to n. reversed to add them to divs array in crescent order
         for(long i = rad; i >= 1; i--){
             if(n % i == 0 && n/i != i){
                 divs.add(n/i);
             }
         }
 
+        long end = System.nanoTime();
+
+        //checks if n is prime
         if(divs.size() == 2){
             System.out.println("Il numero è primo");
         }
@@ -54,12 +63,16 @@ class Main {
             System.out.println("Il numero ha " + divs.size() + " divisori");
         }
 
+        //prints every divisor
         for(int i = 0; i < divs.size(); i++){
             System.out.print(divs.get(i) + " ");
         }
+
         System.out.println();
 
-
-
+        //to print elapsed time in nanoseconds, milliseconds and seconds
+        System.out.println("Elapsed time: " + (end - start) + "ns");
+        System.out.println("Elapsed time: " + ((end - start) / 1000000) + "ms");
+        System.out.println("Elapsed time: " + ((end - start) / 1000000000) + "s");
     }
 }
